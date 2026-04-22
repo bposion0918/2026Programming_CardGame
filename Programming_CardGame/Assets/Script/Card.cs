257cbc3d360636690ad1d3729f0b43eee669455e
bposion0918 <b.posion.0918@gmail.com>
2026-04-23 02:34:49 +0900

    프로모션 완?성

--- Programming_CardGame/Assets/Script/Card.cs
+++ Programming_CardGame/Assets/Script/Card.cs
@@ -1,16 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 
 public class Card : MonoBehaviour
 {
     public TextMeshProUGUI card;
-    private int cardNum;
+    public int cardNum;
     public float rotateSpeed;
-    public bool isClick = false;
+
+    public bool isFront = false;
+
+    public bool isMatched = false;
+
     public Quaternion flipRotation = Quaternion.Euler(0, 180.0f, 0);
     public Quaternion originRotation = Quaternion.Euler(0, 0, 0);
 
+    public CardGame cardGame;
 
     void Start()
     {
@@ -18,7 +25,7 @@ public class Card : MonoBehaviour
 
     void Update()
     {
-        if (isClick)
+        if (isFront)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, flipRotation, rotateSpeed *  Time.deltaTime);
         }
@@ -30,14 +37,26 @@ public class Card : MonoBehaviour
 
     public void Clickcard()
     {
-        isClick = !isClick;
+        if (!isMatched)
+        {
+            cardGame.OnClickCard(this);
+        }
     }
-
     public void SetCardNum(int newNum)
     {
         card = GetComponentInChildren<TextMeshProUGUI>();
         cardNum = newNum;
 
         card.text = cardNum.ToString();
+    }
+
+    public void ChangeColor(Color newColor)
+    {
+        GetComponent<Image>().color = newColor;
+    }
+
+    public void Flip(bool isFront)
+    {
+        this.isFront = isFront;
     }
 }
--- Programming_CardGame/Assets/Script/CardGame.cs
+++ Programming_CardGame/Assets/Script/CardGame.cs
@@ -1,18 +1,120 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class CardGame : MonoBehaviour
 {
     public List<Card> cards;
+    public List<Sprite> sprites;
+
+    private Card firstCard = null;
+    private Card secondCard = null;
+    private bool isChecking = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartGame();
+
+    }
+
+    private void StartGame()
+    {
+        List<int> randomPairNum = GeneratePairNum(cards.Count);
+
+        int loopCount = Mathf.Min(cards.Count, randomPairNum.Count);
+
+        for (int i = 0; i < cards.Count; i++)
+        {
+            cards[i].SetCardNum(randomPairNum[i]);
+        }
+        if (cards.Count > randomPairNum.Count)
+        {
+
+        }
+    }
+
+    private void CheckCard()
+    {
+        isChecking = true;
+
+        if(firstCard.cardNum == secondCard.cardNum)
+        {
+
+            firstCard.isMatched = true;
+            secondCard.isMatched = true;
+
+            firstCard.ChangeColor(Color.yellow);
+            secondCard.ChangeColor(Color.yellow);
+
+            firstCard = null;
+            secondCard = null;
+
+            isChecking = false;
+        }
+        else
+        {
+            Invoke("HideCard", 2.0f);
+        }
+    }
+    private void HideCard()
+    {
+        firstCard.isFront = false;
+        secondCard.isFront = false;
+
+        isChecking = false;
+
+        firstCard = null;
+        secondCard= null;
+    }
+
+    public void OnClickCard(Card Card)
+    {
+        if (isChecking)
+        {
+            return;
+        }
+        if (firstCard == null)
+        {
+            firstCard = Card;
+            firstCard.Flip(true);
+        }
+        else
+        {
+            secondCard = Card;
+            secondCard.Flip(true);
+        }
+
+        if(firstCard != null && secondCard != null)
+        {
+            CheckCard();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private List<int> GeneratePairNum(int cardCount)
     {
-        
+        int pairCount = cardCount / 2;
+        List<int> newCardNum = new List<int>();
+
+        for(int i = 0; i < pairCount; i++)
+        {
+            newCardNum.Add(i);
+            newCardNum.Add(i);
+        }
+
+
+        // [0] [0] [1] [1] [2] [2] [3] [3] [4] [4]
+
+        for(int i = newCardNum.Count -1; i > 0; i--)
+        {
+            int rnd = Random.Range(0, i + 1);
+            int temp = newCardNum[i];
+            newCardNum[i] = newCardNum[rnd];
+            newCardNum[rnd] = temp;
+
+        }
+
+        return newCardNum;
     }
 }
