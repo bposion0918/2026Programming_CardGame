f75cc42ac97ab75b37e4793c9fd6f4ad36e701c3
bposion0918 <b.posion.0918@gmail.com>
2026-04-22 18:40:20 +0900

    카드 회전 추가 및 랜덤 숫자

--- Programming_CardGame/Assets/Script/Card.cs
+++ Programming_CardGame/Assets/Script/Card.cs
@@ -6,15 +6,35 @@ public class Card : MonoBehaviour
 {
     public TextMeshProUGUI card;
     private string cardNum;
+    public float rotateSpeed;
+    public bool isClick = false;
+    public Quaternion flipRotation = Quaternion.Euler(0, 180.0f, 0);
+    public Quaternion originRotation = Quaternion.Euler(0, 0, 0);
+
 
     void Start()
     {
-        card = GetComponent<TextMeshProUGUI>();
+        card = GetComponentInChildren<TextMeshProUGUI>();
+
         cardNum = Random.Range(0, 10).ToString();
+
+        card.text = cardNum;
     }
 
     void Update()
     {
-        transform.Rotate(new Vector3(0.0f, 20.0f, 0.0f) * Time.deltaTime);
+        if (isClick)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, flipRotation, rotateSpeed *  Time.deltaTime);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, originRotation, rotateSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Clickcard()
+    {
+        isClick = !isClick;
     }
 }
